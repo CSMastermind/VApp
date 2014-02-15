9d6d6df32a54990107cb3c26f2e2bb143eafc32c
gageames <gra5028@psu.edu>
2014-02-15 14:36:27 -0500

    Minor UI tweaks for profile.

--- VApp/SplitPage.xaml.cs
+++ VApp/SplitPage.xaml.cs
@@ -129,10 +129,10 @@ namespace VApp
             if (this.UsingLogicalPageNavigation()) this.InvalidateVisualState();
 
             // Set the correct user control
-            this.itemDetailGrid.Children.Clear();
+            this.itemDetailGrid.Content = null;
             if (this.itemListView.SelectedItem != null && ((SampleDataItem)this.itemListView.SelectedItem).ControlType != null)
             {
-                this.itemDetailGrid.Children.Add(Activator.CreateInstance(((SampleDataItem)this.itemListView.SelectedItem).ControlType) as UIElement);
+                this.itemDetailGrid.Content = Activator.CreateInstance(((SampleDataItem)this.itemListView.SelectedItem).ControlType) as UIElement;
             }
         }
 
