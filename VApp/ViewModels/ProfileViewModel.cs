d30f593df115eefbbc9953c5ef266804e51cfcac
gageames <gra5028@psu.edu>
2014-02-15 11:15:55 -0500

    Added a starter user control for profile information.

---
+++ VApp/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VApp.Common;
+
+namespace VApp.ViewModels
+{
+    public class ProfileViewModel : DefaultNotifyPropertyChanged
+    {
+        private string firstName;
+
+        private string middleName;
+
+        private string lastName;
+
+        private string alias;
+
+        private string occupation;
+
+        public ProfileViewModel()
+        {
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                if (value != null && !value.Equals(this.firstName))
+                {
+                    this.firstName = value;
+                    this.NotifyPropertyChanged("FirstName");
+                }
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return this.middleName;
+            }
+
+            set
+            {
+                if (value != null && !value.Equals(this.middleName))
+                {
+                    this.middleName = value;
+                    this.NotifyPropertyChanged("MiddleName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            set
+            {
+                if (value != null && !value.Equals(this.lastName))
+                {
+                    this.lastName = value;
+                    this.NotifyPropertyChanged("LastName");
+                }
+            }
+        }
+
+        public string Alias
+        {
+            get
+            {
+                return this.alias;
+            }
+
+            set
+            {
+                if (value != null && !value.Equals(this.alias))
+                {
+                    this.alias = value;
+                    this.NotifyPropertyChanged("Alias");
+                }
+            }
+        }
+
+        public string Occupation
+        {
+            get
+            {
+                return this.occupation;
+            }
+
+            set
+            {
+                if (value != null && !value.Equals(this.occupation))
+                {
+                    this.occupation = value;
+                    this.NotifyPropertyChanged("Occupation");
+                }
+            }
+        }
+
+    }
+}
