eaa2a028cef9167b2c742345d98482742a941831
Chris Seltzer <cts164@psu.edu>
2014-02-16 09:48:41 -0500

    Added document search and add controls.

---
+++ VApp/UserControls/AddDocumentUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VApp.UserControls
+{
+    public sealed partial class AddDocumentUserControl : UserControl
+    {
+        public AddDocumentUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.CommitButtonText = "Add";
+            picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            picker.FileTypeFilter.Add(".pdf");
+            picker.FileTypeFilter.Add(".doc");
+            picker.FileTypeFilter.Add(".docx");
+            StorageFile file = await picker.PickSingleFileAsync();
+
+            this.FileNameTextBox.Text = string.Format("Successfully added {0}.", file.DisplayName);
+        }
+    }
+}
